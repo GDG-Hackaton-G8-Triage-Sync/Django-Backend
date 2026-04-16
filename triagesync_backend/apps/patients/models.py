045324358b6c83fba613c2b532a4318b89d02802
arsema619 <arsemamenberu16@gmail.com>
2026-04-16 02:18:39 -0700

    Update models.py

--- triagesync_backend/apps/patients/models.py
+++ triagesync_backend/apps/patients/models.py
@@ -2,9 +2,10 @@ from django.conf import settings
 from django.db import models
 
 
-class PatientSubmission(models.Model):
-    patient = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+class Patient(models.Model):
+    name = models.CharField(max_length=100)
     symptoms = models.TextField()
+    priority = models.CharField(max_length=10)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self) -> str:
