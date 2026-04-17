b5b65b6c102df397a740f6f0c646c8509d3c1fa5
arsema619 <arsemamenberu16@gmail.com>
2026-04-17 11:40:55 -0700

    Update serializers.py

--- triagesync_backend/apps/authentication/serializers.py
+++ triagesync_backend/apps/authentication/serializers.py
@@ -1,15 +1,24 @@
-from django.contrib.auth import get_user_model
 from rest_framework import serializers
-
-User = get_user_model()
-
+from .models import User
+from django.contrib.auth import authenticate
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
-
     class Meta:
         model = User
-        fields = ("id", "username", "email", "password", "role")
+        fields = ['username', 'password', 'role']
+        extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        return User.objects.create_user(**validated_data)
+        user = User.objects.create_user(**validated_data)
+        return user
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def validate(self, data):
+        user = authenticate(**data)
+        if not user:
+            raise serializers.ValidationError("Invalid credentials")
+        return user
