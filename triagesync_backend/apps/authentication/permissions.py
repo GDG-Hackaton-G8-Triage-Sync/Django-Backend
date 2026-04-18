e271fee334e3b83e4ca7c44ee73a4fb5a44f00a2
arsema619 <arsemamenberu16@gmail.com>
2026-04-18 16:50:23 -0700

    Update permissions.py

--- triagesync_backend/apps/authentication/permissions.py
+++ triagesync_backend/apps/authentication/permissions.py
@@ -4,6 +4,9 @@ class IsDoctor(BasePermission):
     def has_permission(self, request, view):
         return request.user.is_authenticated and request.user.role == 'doctor'
 
+class IsNurse(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.is_nurse()
 
 class IsAdmin(BasePermission):
     def has_permission(self, request, view):
