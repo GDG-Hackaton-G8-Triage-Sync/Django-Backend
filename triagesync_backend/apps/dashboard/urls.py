d206c6017d939399792a07b8fb8f46065854cacb
kidus mulugeta <kmulugeta036@gmail.com>
2026-04-19 15:14:50 +0300

    links are added

--- triagesync_backend/apps/dashboard/urls.py
+++ triagesync_backend/apps/dashboard/urls.py
@@ -1,22 +1,17 @@
 from django.urls import path
-
-
 from .views import (
-    DashboardPatientListView,
-    StaffPatientStatusUpdateView,
+    StaffPatientQueueView,
+    UpdatePatientStatusView,
     AdminOverviewView,
     AdminAnalyticsView,
-    StaffQueueView,
-    StaffPatientDetailView,
-    StaffPriorityOverrideView,
 )
 
 urlpatterns = [
-    path("patients/", DashboardPatientListView.as_view(), name="dashboard-patients"),
-    path("staff/queue/", StaffQueueView.as_view(), name="staff-queue"),
-    path("staff/patient/<str:session_id>/", StaffPatientDetailView.as_view(), name="staff-patient-detail"),
-    path("staff/patient/<str:session_id>/override/", StaffPriorityOverrideView.as_view(), name="staff-priority-override"),
-    path("staff/patient/<int:id>/status/", StaffPatientStatusUpdateView.as_view(), name="staff-patient-status-update"),
-    path("admin/overview/", AdminOverviewView.as_view(), name="admin-overview"),
-    path("admin/analytics/", AdminAnalyticsView.as_view(), name="admin-analytics"),
+    # STAFF
+    path("staff/patients/", StaffPatientQueueView.as_view()),
+    path("staff/patient/<int:id>/status/", UpdatePatientStatusView.as_view()),
+
+    # ADMIN
+    path("admin/overview/", AdminOverviewView.as_view()),
+    path("admin/analytics/", AdminAnalyticsView.as_view()),
 ]
