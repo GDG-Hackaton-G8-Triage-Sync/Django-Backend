524295a77313b3d164c97e3cf7905093d2eaed30
kidus mulugeta <kmulugeta036@gmail.com>
2026-04-19 14:24:54 +0300

    updated some of the fields in the serializer

--- triagesync_backend/apps/dashboard/serializers.py
+++ triagesync_backend/apps/dashboard/serializers.py
@@ -1,23 +1,19 @@
 from rest_framework import serializers
+from apps.patients.models import PatientSubmission
 
-from triagesync_backend.apps.patients.models import PatientSubmission
+
 class DashboardPatientSerializer(serializers.ModelSerializer):
-    # 🔁 Rename fields to match API contract
+    # rename field to match API contract ("description" instead of "symptoms")
     description = serializers.CharField(source="symptoms")
-    patient_name = serializers.CharField(source="patient.name")
 
     class Meta:
         model = PatientSubmission
         fields = (
             "id",
-            "patient_name",
             "description",
             "priority",
             "urgency_score",
             "condition",
             "status",
-            "photo_name",
-            "verified_by",
-            "verified_at",
             "created_at",
         )
