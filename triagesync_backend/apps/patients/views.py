a26c627861642c098cadd209a1f1b922eee743af
arsema619 <arsemamenberu16@gmail.com>
2026-04-18 17:04:42 -0700

    Update views.py

--- triagesync_backend/apps/patients/views.py
+++ triagesync_backend/apps/patients/views.py
@@ -1,11 +1,12 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
 from .serializers import PatientSubmissionSerializer
+from rest_framework.permissions import IsAuthenticated
 
 
 class PatientSubmissionView(APIView):
+    permission_classes = [IsAuthenticated] 
     def post(self, request):
         serializer = PatientSubmissionSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
