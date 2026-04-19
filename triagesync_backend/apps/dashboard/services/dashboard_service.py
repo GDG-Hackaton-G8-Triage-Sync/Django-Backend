ea52bed99bf099ae36e48315cdf0fd7eb51f528a
kidus mulugeta <kmulugeta036@gmail.com>
2026-04-19 14:52:21 +0300

    dashboard services are added

--- triagesync_backend/apps/dashboard/services/dashboard_service.py
+++ triagesync_backend/apps/dashboard/services/dashboard_service.py
@@ -1,6 +1,5 @@
 from apps.patients.models import PatientSubmission
 from django.db.models import Avg, Count
-from django.utils import timezone
 
 
 def get_patient_queue(priority=None, status=None):
@@ -61,18 +60,3 @@ def get_admin_analytics():
             .order_by("-count")[:3]
         ),
     }
-
-def update_priority(patient, priority):
-    patient.priority = priority
-    patient.save()
-    return patient
-
-
-def verify_patient(patient, user):
-    if patient.verified_at:
-        return None
-
-    patient.verified_by = user.username
-    patient.verified_at = timezone.now()
-    patient.save()
-    return patient
