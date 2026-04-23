ece6cfbc0119b52f1b2ec0ad453d5aac70995ffc
samrawit sissay <samrawitsissayg.michael@gmail.com>
2026-04-23 14:07:10 +0300

    submission_trigger

--- triagesync_backend/apps/dashboard/apps.py
+++ triagesync_backend/apps/dashboard/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class DashboardConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "triagesync_backend.apps.dashboard"
+    name = "apps.dashboard"
