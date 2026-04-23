8562c555decead92a494c8504fc7ee781a662ff8
Nardos276 <nardiassefa97@gmail.com>
2026-04-23 22:52:40 +0300

    Member 7: data layer queries aligned with updated API

--- data_layer/settings.py
+++ data_layer/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'triage',
+    'authentication',
+    'patients',
 ]
 
 MIDDLEWARE = [
@@ -116,3 +118,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+AUTH_USER_MODEL = 'authentication.User'
