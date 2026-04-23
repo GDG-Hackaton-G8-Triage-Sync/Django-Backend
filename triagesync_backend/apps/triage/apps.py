9fec8542a582061a044b76b67f8dfba7cdc40599
samrawit sissay <samrawitsissayg.michael@gmail.com>
2026-04-23 14:07:10 +0300

    submission_trigger

--- triagesync_backend/apps/triage/apps.py
+++ triagesync_backend/apps/triage/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class TriageConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "triagesync_backend.apps.triage"
+    name = "apps.triage"
