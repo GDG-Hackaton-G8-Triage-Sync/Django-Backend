cee68f095e44bf72f702a46f3cfc4b6933a34f4b
kidus mulugeta <kmulugeta036@gmail.com>
2026-04-25 15:39:02 +0300

    some fields are added to match the api contract

--- triagesync_backend/apps/dashboard/serializers.py
+++ triagesync_backend/apps/dashboard/serializers.py
@@ -3,17 +3,22 @@ from apps.patients.models import PatientSubmission
 
 
 class DashboardPatientSerializer(serializers.ModelSerializer):
-    # rename field to match API contract ("description" instead of "symptoms")
+    # 🔁 Rename fields to match API contract
     description = serializers.CharField(source="symptoms")
+    patient_name = serializers.CharField(source="patient.name")
 
     class Meta:
         model = PatientSubmission
         fields = (
             "id",
+            "patient_name",
             "description",
             "priority",
             "urgency_score",
             "condition",
             "status",
+            "photo_name",
+            "verified_by",
+            "verified_at",
             "created_at",
         )
