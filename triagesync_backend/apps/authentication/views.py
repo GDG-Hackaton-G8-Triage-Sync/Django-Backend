2a4cc461739e62c0721de628fa59d3d027e32567
arsema619 <arsemamenberu16@gmail.com>
2026-04-25 06:48:08 -0700

    Update views.py

--- triagesync_backend/apps/authentication/views.py
+++ triagesync_backend/apps/authentication/views.py
@@ -1,9 +1,10 @@
 from rest_framework.views import APIView
-from rest_framework.response import Response
 from rest_framework import status
+
 from .serializers import RegisterSerializer, LoginSerializer
 from .services.auth_service import get_tokens_for_user
 from apps.core.response import success_response, error_response
+
 
 class RegisterView(APIView):
     def post(self, request):
@@ -16,10 +17,9 @@ class RegisterView(APIView):
             )
 
         user = serializer.save()
-
         tokens = get_tokens_for_user(user)
 
-        return Response({
+        return success_response({
             "user": serializer.data,
             "tokens": tokens
         })
@@ -28,11 +28,16 @@ class RegisterView(APIView):
 class LoginView(APIView):
     def post(self, request):
         serializer = LoginSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
+
+        if not serializer.is_valid():
+            return error_response(
+                {"message": "Invalid username or password"},
+                status=status.HTTP_401_UNAUTHORIZED
+            )
 
         user = serializer.validated_data
         tokens = get_tokens_for_user(user)
 
-        return Response({
+        return success_response({
             "tokens": tokens
         })
