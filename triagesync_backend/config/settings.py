02643d3832ac11ae3b755672ca06c6ffaf8b6f8f
Abel Mekonnen <abelmekonen247@gmail.com>
2026-05-02 01:11:22 +0300

    installed and added daphne in the installed apps
    updated the requirements.txt

--- triagesync_backend/config/settings.py
+++ triagesync_backend/config/settings.py
@@ -53,6 +53,7 @@ if render_hostname:
 ALLOWED_HOSTS = list(dict.fromkeys([host for host in normalized_allowed_hosts if host]))
 
 INSTALLED_APPS = [
+    "daphne",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
